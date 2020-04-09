e67dc716027fc0ce903b694c2a3e7a51a862c296
vitorss <oi.vitorss@gmail.com>
2020-04-08 23:17:27 -0300

    edit some colors and logo

--- src/js/sticky.js
+++ src/js/sticky.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".navigation");
-    navbar.classList.toggle("sticky", window.scrollY > 0);
+    navbar.classList.toggle("sticky", window.scrollY > 1);
 });
