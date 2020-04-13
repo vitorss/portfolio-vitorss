7c68aa2f971874709cd415f428dd37963e46fe9d
vitorss <oi.vitorss@gmail.com>
2020-04-13 15:15:03 -0300

    add gallery

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -2,7 +2,7 @@ function typewriter (elemento) {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 85 * i)
+        setTimeout(() => elemento.innerHTML += letra, 95 * i)
     });
 }
 
